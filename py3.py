efe482d5278b5c2f11f99cb89e21b42df39630ce
mohi2 <silentsongmea39@yahoo.com>
2024-07-02 21:40:23 +0430

    |-x|=x oder |x|=x

---
+++ py3.py
@@ -0,0 +1,9 @@
+i=1
+while(i>0):
+    Nummer1=int(input("\n Schicken Sie Nummer bitte:"))
+    if Nummer1>=0:
+        print(Nummer1)
+    else:
+        Nummer1=Nummer1*-1
+        print(Nummer1)
+    i+=1
