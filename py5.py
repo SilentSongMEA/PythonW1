e0117ed04b3da003778483459208d991afc2161a
mohi2 <silentsongmea39@yahoo.com>
2024-07-02 23:30:34 +0430

    Umfang Das Kreises und Kreisfläche

---
+++ py5.py
@@ -0,0 +1,9 @@
+i=1
+Kreisfläche=1
+UmfangDesKreises=1
+while(i>0):
+    Nummer1=int(input("Geben Sie den Kreisradius an:"))
+    Kreisfläche=(Nummer1*2)*3.14
+    UmfangDesKreises=(Nummer1*Nummer1)*3.14
+    print(f"\n Kreisfläche:{Kreisfläche} \n Umfang Das Kreises:{UmfangDesKreises}")
+    i+=1
